cb81507c4ddd249f9bab29a88dc711a242d3b18d
Archil Imnadze <archil@imnadze.ge>
2015-07-30 18:43:34 +0400

    script to clean pid, err and out files

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd `dirname $BASH_SOURCE`
+rm -f server.err server.out server.pid
--- stop.sh
+++ stop.sh
@@ -3,5 +3,5 @@ cd `dirname $BASH_SOURCE`
 if [ -f server.pid ]
 then
     kill `cat server.pid` 2> /dev/null
-    rm -f server.err server.out server.pid
+    ./clean.sh
 fi
